59dc67475fbebbb4f763fde89ee36531315e1f80
password123456 <sendmefuture@gmail.com>
2022-04-25 08:17:35 +0900

    Update send_to_telegram_photo.py

--- send_to_telegram_photo.py
+++ send_to_telegram_photo.py
@@ -21,7 +21,7 @@ class Bcolors:
 
 def send_to_telegram_photo(file_name, file_caption, chat_id):
     token = '$your_bot_token_id$'
-    url = 'https://api.telegram.org/bot%s/sendMessage' % token
+    url = 'https://api.telegram.org/bot%s/sendPhoto' % token
 
     header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) '
                             'Chrome/49.0.2623.112 Safari/537.36'}
